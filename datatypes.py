12f241b84a0ae80b1c04be2feec9403767bf8cbf
UniOp <growdon4321@gamil.com>
2025-10-09 08:31:19 -0700

    commit

---
+++ datatypes.py
@@ -0,0 +1,21 @@
+Name = "Shashwata"
+age = "16"
+is_student = "True"
+weight = "72"
+
+print ("name :", Name)
+print ("data type of name is", type (Name))
+print ("Age :", age)
+print ("data type of Age is ", type (age))
+print ("is_student :", is_student)
+print ("data type of is_student is ",type( is_student))
+print ("weight :", weight)
+print ("data type of weight is ", type (weight))
+
+print("\n after type casting...")
+age = str(age)
+print(age)
+print("data type of age is", type (age))
+weight = int(weight)
+print(weight)
+print("data type of weight is ", type(weight))
---
+++ oddeven.py
@@ -0,0 +1,6 @@
+number = int(input("enter the number"))
+print("number to be checked :", 14)
+if number%2==0 : 
+ print("this is an even number") 
+else:
+ print("this is an odd number")
