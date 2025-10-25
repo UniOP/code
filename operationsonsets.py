7b104e0284b7819ea9642fb8014c5f538faabe34
UniOp <growdon4321@gamil.com>
2025-10-25 03:30:33 -0700

    commit

---
+++ operationsonsets.py
@@ -0,0 +1,15 @@
+my_set = {1, 2, 3, 4, 5, 6, 3, 4, 5}
+print("set :", my_set)
+
+my_set.add(5)
+print("updated set :", my_set)
+
+set1 = my_set
+set2 = {2, 2, 5, 5, 3}
+
+print("\nset 1",set1)
+print("set 2", set2)
+print("difference")
+print(set1.difference(set2))
+print("symmeteric difference")
+print(set1.symmetric_difference(set2))
---
+++ setintersection.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("orignal set elements:")
+print(setx)
+print(sety)
+print("\nintrsection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
